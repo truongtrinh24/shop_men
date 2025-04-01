59049e0d3f7bef06be1f200916e2488c22050c13
truong <trinhtruong25303@gmail.com>
2025-04-01 19:42:12 +0700

    dlt admin

--- app/controllers/About.php
+++
@@ -1,9 +0,0 @@
-<?php
-class About extends Controller {
-    public function index() {
-        $this->data['content'] = 'blocks/clients/about';
-        $this->data['sub_content']= [];
-        $this->render('layouts/client_layout', $this->data);
-    }
-}
-?>
